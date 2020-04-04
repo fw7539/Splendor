4a29c635285306ea49d99e1e8fc66c24b1d64264
Fred Worley <fred_worley@yahoo.com>
2020-04-04 16:50:42 -0700

    Updated code for IP address of sever - try local name, then try hard IP address of the house

--- Splendor_client.py
+++ Splendor_client.py
@@ -1,12 +1,16 @@
-import socket               # Import socket module
+import socket  # Import socket module
 
-s = socket.socket()         # Create a socket object
+s = socket.socket()  # Create a socket object
 
-host = socket.gethostname() # Get local machine name
-if (host != "FW-CodingLaptop"):         #  Just for testing, running server on my laptop
-     host = "73.71.247.208"  # Our house's IP address seen from outside
-port = 61111                # Reserve a port for your service.
+port = 61111  # Reserve a port for your service.
+server_local_name = 'FW-CodingLaptop'
+server_remote_IP_addr = '73.71.247.208'
+try:
+    server_name = server_local_name
+    s = socket.create_connection((server_local_name, port))
+except:
+    server_name = server_remote_IP_addr
+    s = socket.create_connection((server_remote_IP_addr, port))
 
-s.connect((host, port))
-print("I am " + host + ", I received message " + s.recv(1024).decode('ascii'))
+print("Server is '%s', I received message '%s'" % (server_name, s.recv(1024).decode('ascii')))
 s.close()
