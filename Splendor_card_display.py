6178318831c15fe014dcdab7317afc17a094a3f8
Fred Worley <fred_worley@yahoo.com>
2020-04-09 15:46:34 -0700

    Created file to hold card display unfo:  size of card, etc., and a class to hold display specifics

---
+++ Splendor_card_display.py
@@ -0,0 +1,37 @@
+"""
+    Splendor Card display information
+
+    This file is intended to be customized for different python graphics packages
+    (e.g. tkinter or pygame)
+
+    To be imported by both server and client
+"""
+#  Constants
+board_height = 800
+board_width = 800
+
+card_height = 150
+card_width = 100
+card_cols = 4
+card_rows = 3
+card_gap = 10
+
+#  pile[i].append((id, x, y, card_width, card_height, card_backs[i], i, j))
+
+class Card_display:
+    def __init__(self, id, x, y, card_width, card_height, fill_color, row=-1, col=-1):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.card_width = card_width
+        self.card_height = card_height
+        self.fill_color = fill_color
+        self.row = row
+        self.col = col
+        #  need a self.id as well
+
+    def __repr__(self):
+        return f'Card_display(id={self.id}, x={self.x}, y={self.y}, card_width={self.card_width}, ' \
+               f'card_height={self.card_height}, fill_color={self.fill_color}, row={self.row}, col={self.col})'
+
+
