01494a8f13b6c2f231145a85daf2bed969994493
Fred Worley <fred_worley@yahoo.com>
2020-04-30 00:30:56 -0700

    Got MultiSprite move working, when you click on the box is moves right and the text moves with it.

--- clickable_rectangles.py
+++ clickable_rectangles.py
@@ -5,6 +5,11 @@ class Canvas(tkinter.Canvas):
         super().__init__(*args, **kw)
         self.shapes_listening_for_callbacks=[]
         self.bind('<Button-1>', self.onClick)
+        # self.bind('<B1-Motion>', self.onDrag)
+    '''
+    ('<B1-Motion>', "'LEFT drag %3d, %3d' % (e.x, e.y)"),
+    ('<B3-Motion>', "'%3d, %3d drag RIGHT' % (e.x, e.y)")
+    '''
 
     def create_rectangle(self, *args, **kw):
         return Rectangle(self, super().create_rectangle(*args, **kw))
@@ -21,13 +26,26 @@ class Canvas(tkinter.Canvas):
             if x1 <= click_x <= x2 and y1 <= click_y <= y2:
                 event.x = click_x - x1
                 event.y = click_y - y1
+                if callback(rect) == 'break':
+                    return 'break'
+
+
+    def onDrag(self, event):
+        if event.x is None:
+            return
+        click_x, click_y = event.x, event.y
+        for rect, callback in self.shapes_listening_for_callbacks:
+            x1, y1, x2, y2 = self.bbox(rect.id)
+            if x1 <= click_x <= x2 and y1 <= click_y <= y2:
+                event.x = click_x - x1
+                event.y = click_y - y1
                 if callback(rect)=='break':
                     return 'break'
-                
+
 
 #  call this Sprite?  or SpriteWrapper?  WidgetWrapper? It covers more than Rectangles.
 class Rectangle:
-    __slots__=('canvas','id')
+    __slots__=('canvas','id', 'text_sprites')
     def __init__(self, canvas, id_):
         self.canvas=canvas
         self.id=id_
@@ -41,36 +59,56 @@ class Rectangle:
     def bbox(self):
         return self.canvas.bbox(self.id)
 
+    def move(self, x, y):
+        self.canvas.move(self.id, x, y)
+        if (hasattr(self, 'text_sprites')):
+            debug_print(f"calling text_sprites.move({x},{y})")
+            self.text_sprites.move(x, y)
 
-def draw_text_on_rectangle(rect:Rectangle, *texts):
-    bbox=rect.bbox()
-    canvas=rect.canvas
 
-    debug_print("inside draw_text..., type of rect is: ", type(rect), " bbox is: ", bbox)
+    #def draw_text_on_rectangle(rect:Rectangle, *texts):
+    def draw_text_on_rectangle(self, *texts):
+        bbox=self.bbox()
+        canvas=self.canvas
 
-    bottom_left_corner=(bbox[0]+11, bbox[3]-10)
-    ids=[]
-    for text in texts:
-        new_id=canvas.create_text(bottom_left_corner, text=text, anchor='sw', font=('helvetica', 16))
-        ids.append(new_id)
-        bbox=new_id.bbox()
+        debug_print("inside draw_text..., type of self is: ", type(self), " bbox is: ", bbox)
 
-        debug_print("inside text loop, text is '", text, "', id is ", new_id, "type of rect is: ", type(rect), " bbox is: ", bbox)
+        bottom_left_corner=(bbox[0]+11, bbox[3]-10)
+        ids=[]
+        for text in texts:
+            new_id=canvas.create_text(bottom_left_corner, text=text, anchor='sw', font=('helvetica', 16))
+            ids.append(new_id)
+            bbox=new_id.bbox()
 
-        bottom_left_corner = bbox[0]+1, bbox[1]+1 # TOP left corner of the text box, which will be the bottom left corner
-        # of the next textbox up.
-    return MultiSprite(canvas, rect.id, *ids)
+            debug_print("inside text loop, text is '", text, "', id is ", new_id, "type of self is: ", type(self), " bbox is: ", bbox)
+
+            bottom_left_corner = bbox[0]+1, bbox[1]+1 # TOP left corner of the text box, which will be the bottom left corner
+            # of the next textbox up.
+        self.text_sprites = MultiSprite(canvas, ids)
+        debug_print("MultiSprite returned ", self.text_sprites)
 
 
 class MultiSprite:
-    def __init__(self, canvas, *ids):
+    def __init__(self, canvas, ids):
         self.canvas=canvas
         self.ids=ids
-    def move(self, x, y):
-        for id in self.ids:
-            self.canvas.move(id, x, y)
 
-debug = False
+        for sprite_id in self.ids:
+            debug_print("in __repr, type of sprite_id is ", type(sprite_id), "s_id is ", sprite_id)
+
+    def move(self, x, y):
+        for sprite_id in self.ids:
+            self.canvas.move(sprite_id.id, x, y)
+
+    def __repr__(self):
+        rv = ""
+        for sprite_id in self.ids:
+            debug_print("in __repr, type of sprite_id is ", type(sprite_id), "s_id is ", sprite_id)
+            rv += str(sprite_id.id) + ", "
+        return rv
+
+
+debug = True
 def debug_print(*args, **kwargs):
     if (debug):
         print(*args, **kwargs)
@@ -82,13 +120,16 @@ if __name__=='__main__':
     canvas=Canvas(window)
     canvas.pack()
     def say_hello(rect):
+        rect.move(10,0)
         print("The rectangle was clicked")
         rect.config(fill='blue')
+        rect.move(10,0)
+
 
     rect1 = canvas.create_rectangle(80, 60, 300, 200, fill='red')
     rect1.bindToClick(say_hello)
     debug_print("type of rect is: ", type(rect1), " bbox is: ", rect1.bbox())
-    draw_text_on_rectangle(rect1, 'Diamond', '2 VP', '1 Ruby', '1 Onyx', '1 Sapphire')
+    rect1.draw_text_on_rectangle('Diamond', '2 VP', '1 Ruby', '1 Onyx', '1 Sapphire')
 
     # canvas.create_text(200, 80, text='card name').bindToClick(say_hello)
     window.mainloop()
