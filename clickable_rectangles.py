a7fa9fbcf16346e4846eaa8cf762048f0b73d530
Fred Worley <fred_worley@yahoo.com>
2020-05-01 11:29:28 -0700

    Updated debug_print() to take a debug level About to update draw_text_on_rectangle to allow for location to be specified (so we can make a card)  - should pass along args for x, y, font, color, etc, or take a tuple of config values for the text Changed name of "rectangle" to "widget" since some "rectangles" are text boxes

--- clickable_rectangles.py
+++ clickable_rectangles.py
@@ -12,10 +12,10 @@ class Canvas(tkinter.Canvas):
     '''
 
     def create_rectangle(self, *args, **kw):
-        return Rectangle(self, super().create_rectangle(*args, **kw))
+        return WidgetWrapper(self, super().create_rectangle(*args, **kw))
 
     def create_text(self, *args, **kw):
-        return Rectangle(self, super().create_text(*args, **kw))
+        return WidgetWrapper(self, super().create_text(*args, **kw))
 
     def onClick(self, event):
         if event.x is None:
@@ -44,7 +44,7 @@ class Canvas(tkinter.Canvas):
 
 
 #  call this Sprite?  or SpriteWrapper?  WidgetWrapper? It covers more than Rectangles.
-class Rectangle:
+class WidgetWrapper:
     __slots__=('canvas','id', 'text_sprites')
     def __init__(self, canvas, id_):
         self.canvas=canvas
@@ -62,30 +62,53 @@ class Rectangle:
     def move(self, x, y):
         self.canvas.move(self.id, x, y)
         if (hasattr(self, 'text_sprites')):
-            debug_print(f"calling text_sprites.move({x},{y})")
+            debug_print('multi_sprite', f"calling text_sprites.move({x},{y})")
             self.text_sprites.move(x, y)
 
 
     #def draw_text_on_rectangle(rect:Rectangle, *texts):
-    def draw_text_on_rectangle(self, *texts):
+    def draw_text_on_widget(self, *texts):
         bbox=self.bbox()
         canvas=self.canvas
 
-        debug_print("inside draw_text..., type of self is: ", type(self), " bbox is: ", bbox)
+        debug_print('rectangle_display', "inside draw_text..., type of self is: ", type(self), " bbox is: ", bbox)
 
-        bottom_left_corner=(bbox[0]+11, bbox[3]-10)
+        bottom_left_corner=(bbox[0]+11, bbox[3]-10) #  bbox returns 1 outside the object
+        #  added back a "reasonble indent", trial/error values
         ids=[]
         for text in texts:
             new_id=canvas.create_text(bottom_left_corner, text=text, anchor='sw', font=('helvetica', 16))
             ids.append(new_id)
             bbox=new_id.bbox()
 
-            debug_print("inside text loop, text is '", text, "', id is ", new_id, "type of self is: ", type(self), " bbox is: ", bbox)
+            debug_print('rectangle_display', "text loop, text is '", text, "', id is ", new_id, "type of self is: ", type(self), " bbox is: ", bbox)
 
             bottom_left_corner = bbox[0]+1, bbox[1]+1 # TOP left corner of the text box, which will be the bottom left corner
             # of the next textbox up.
         self.text_sprites = MultiSprite(canvas, ids)
-        debug_print("MultiSprite returned ", self.text_sprites)
+        debug_print('multi_sprite', "MultiSprite returned ", self.text_sprites)
+
+    def add_text_to_widget(self, text, x, y):
+        # TODO:  change x, y into optional parameters that will mimic draw_text_on_widget behavior
+        bbox=self.bbox()
+        canvas=self.canvas
+
+        debug_print('rectangle_display', "inside draw_text..., type of self is: ", type(self), " bbox is: ", bbox)
+
+        bottom_left_corner=(bbox[0]+11, bbox[3]-10) #  bbox returns 1 outside the object
+                                                    #  added back a "reasonble indent", trial/error values
+        ids=[]
+        for text in texts:
+            new_id=canvas.create_text(bottom_left_corner, text=text, anchor='sw', font=('helvetica', 16))
+            ids.append(new_id)
+            bbox=new_id.bbox()
+
+            debug_print('rectangle_display', "text loop, text is '", text, "', id is ", new_id, "type of self is: ", type(self), " bbox is: ", bbox)
+
+            bottom_left_corner = bbox[0]+1, bbox[1]+1 # TOP left corner of the text box, which will be the bottom left corner
+            # of the next textbox up.
+        self.text_sprites = MultiSprite(canvas, ids)
+        debug_print('multi_sprite', "MultiSprite returned ", self.text_sprites)
 
 
 class MultiSprite:
@@ -94,7 +117,7 @@ class MultiSprite:
         self.ids=ids
 
         for sprite_id in self.ids:
-            debug_print("in __repr, type of sprite_id is ", type(sprite_id), "s_id is ", sprite_id)
+            debug_print('multi_sprite', "in __repr, type of sprite_id is ", type(sprite_id), "s_id is ", sprite_id)
 
     def move(self, x, y):
         for sprite_id in self.ids:
@@ -103,14 +126,18 @@ class MultiSprite:
     def __repr__(self):
         rv = ""
         for sprite_id in self.ids:
-            debug_print("in __repr, type of sprite_id is ", type(sprite_id), "s_id is ", sprite_id)
+            debug_print('multi_sprite', "in __repr, type of sprite_id is ", type(sprite_id), "s_id is ", sprite_id)
             rv += str(sprite_id.id) + ", "
         return rv
 
 
-debug = True
-def debug_print(*args, **kwargs):
-    if (debug):
+active_debug_levels = (
+    # 'multi_sprite',
+    # 'rectangle_display',
+)
+
+def debug_print(debug_level, *args, **kwargs):
+    if (debug_level in active_debug_levels):
         print(*args, **kwargs)
 
 
@@ -128,8 +155,8 @@ if __name__=='__main__':
 
     rect1 = canvas.create_rectangle(80, 60, 300, 200, fill='red')
     rect1.bindToClick(say_hello)
-    debug_print("type of rect is: ", type(rect1), " bbox is: ", rect1.bbox())
-    rect1.draw_text_on_rectangle('Diamond', '2 VP', '1 Ruby', '1 Onyx', '1 Sapphire')
+    debug_print('rectangle_display', "type of rect is: ", type(rect1), " bbox is: ", rect1.bbox())
+    rect1.draw_text_on_widget('Diamond', '2 VP', '1 Ruby', '1 Onyx', '1 Sapphire')
 
     # canvas.create_text(200, 80, text='card name').bindToClick(say_hello)
     window.mainloop()
